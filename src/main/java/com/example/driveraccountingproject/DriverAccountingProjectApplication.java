f6c39c64ee10bc6042ba49f2f78d1c74c5fc0cc5
Dzmitry Antsipin <dzmitry.antsipin@gmail.com>
2023-06-23 10:20:28 +0200

    initial commit

---
+++ src/main/java/com/example/driveraccountingproject/DriverAccountingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.driveraccountingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriverAccountingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DriverAccountingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/driveraccountingproject/DriverAccountingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.driveraccountingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverAccountingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
