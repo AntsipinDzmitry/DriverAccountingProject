c5a20ca21c2df68af1b5deb5d84e42dd68da055c
Dzmitry Antsipin <dzmitry.antsipin@gmail.com>
2023-06-25 16:37:25 +0200

    pom was changed

--- src/main/java/com/example/driveraccountingproject/config/SwaggerConfig.java
+++ src/main/java/com/example/driveraccountingproject/config/SwaggerConfig.java
@@ -4,10 +4,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
